e54c83bbc8e7b7ce1f78722d1fa2d154d4b2bd97
Gonzalo Alvarez Garcia <48485230+gonalvgar@users.noreply.github.com>
2021-05-16 00:27:57 +0200

    case positive and negative done!
    authenticated.consumer

--- src/test/java/acme/testing/authenticated/consumer/CreateTests.java
+++ src/test/java/acme/testing/authenticated/consumer/CreateTests.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 public class CreateTests extends AcmePlannerTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/sign-up/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-    public void positiveBecomeConsumer(final String username, final String password, final String name, final String surname, final String email,final String phone, final String company, final String sector) {
-        super.signUp(username, password, name, surname, email, "");
+    public void positiveBecomeConsumer(final String username, final String password, final String name, final String surname, final String email, final String company, final String sector, final String phone) {
+        super.signUp(username, password, name, surname, email, phone);
         super.signIn(username, password);
         super.clickOnMenu("Account", "Become a consumer");
         super.fillInputBoxIn("company", company);
@@ -16,6 +16,18 @@ public class CreateTests extends AcmePlannerTest {
 
         super.clickOnSubmitButton("Register");
         super.checkSimplePath("/master/welcome");
+
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/authenticated.consumer/negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+    public void negativeBecomeConsumer(final String username, final String password, final String name, final String surname, final String email, final String company, final String sector, final String phone) {
+        super.signUp(username, password, name, surname, email, phone);
+        super.signIn(username, password);
+        super.clickOnMenu("Account", "Become a consumer");
+        super.fillInputBoxIn("company", company);
+        super.fillInputBoxIn("sector", sector);
+        super.clickOnSubmitButton("Register");
+        super.checkErrorsExist();
 
     }
     }
