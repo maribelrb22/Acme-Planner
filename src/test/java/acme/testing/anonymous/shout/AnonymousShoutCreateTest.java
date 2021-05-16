5c0e523924fe3dcc94b9eb818601e4852710297a
Evelyn Gisele Yugsi Yugsi <eveyugyug@alum.us.es>
2021-05-16 20:02:18 +0200

    Test-6 almost finished

--- src/test/java/acme/testing/anonymous/shout/AnonymousShoutCreateTest.java
+++ src/test/java/acme/testing/anonymous/shout/AnonymousShoutCreateTest.java
@@ -1,7 +1,52 @@
+
 package acme.testing.anonymous.shout;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.AcmePlannerTest;
 
-public class AnonymousShoutCreateTest extends AcmePlannerTest{
+public class AnonymousShoutCreateTest extends AcmePlannerTest {
+	// Lifecycle management ---------------------------------------------------
 
+	// Test cases -------------------------------------------------------------
+	//This test shows every shout in the data base
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/shout/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void createPositive(final int recordIndex, final String moment, final String author, final String text, final String info) {
+		super.clickOnMenu("Anonymous", "Create shout");
+		
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("text", text);
+		super.fillInputBoxIn("info", info);
+		
+		super.clickOnSubmitButton("Shout!");
+		
+		super.clickOnMenu("Anonymous", "Shouts list");
+
+		super.checkColumnHasValue(recordIndex, 0, moment);
+		super.checkColumnHasValue(recordIndex, 1, author);
+		super.checkColumnHasValue(recordIndex, 2, text);
+		
+
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/shout/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void createNegative(final int recordIndex, final String moment, final String author, final String text, final String info) {
+		super.clickOnMenu("Anonymous", "Create shout");
+		
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("text", text);
+		super.fillInputBoxIn("info", info);
+		
+		super.clickOnSubmitButton("Shout!");
+		
+		super.checkErrorsExist();
+		
+
+	}
 }
