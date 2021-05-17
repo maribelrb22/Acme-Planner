f8f540f6837aa6340ba753fe70b1a94d46d97dd1
manmunesp12 <manmunesp1@alum.us.es>
2021-05-17 20:10:19 +0200

    Test comment

--- src/test/java/acme/testing/anonymous/task/AnonymousTaskListTest.java
+++ src/test/java/acme/testing/anonymous/task/AnonymousTaskListTest.java
@@ -12,21 +12,25 @@ public class AnonymousTaskListTest extends AcmePlannerTest{
 	
     // Test cases -------------------------------------------------------------
 	
-	// This test case checks the correct vision of the unfinished publics tasks.
+	// Este caso comprueba el correcto funcionamiento del listado de las tareas no finalizadas públicas y sus detalles
 		@ParameterizedTest
 		@CsvFileSource(resources = "/anonymous/task/list.csv", encoding = "utf-8", numLinesToSkip = 1)
 		@Order(10)	
 		public void list(final int recordIndex, final String title, final String begin, final String end,
 			final String description, final String link, final String isPublic, final String workload,final String executionPeriod) {
 			
+			//Accedemos al listado de las tareas desde el menus de anonimo 
 			super.clickOnMenu("Anonymous", "Tasks list");
 			
+			//Comprueba que las columnas del listado están correctamente
 			super.checkColumnHasValue(recordIndex, 0, title);
 			super.checkColumnHasValue(recordIndex, 1, executionPeriod);
 			super.checkColumnHasValue(recordIndex, 2, workload);
 			
+			//Accedemos a una tarea en cuestión
 			super.clickOnListingRecord(recordIndex);
 			
+			//Comprobamos los detalles de la tarea seleccionada anteriormente
 			super.checkInputBoxHasValue("title", title);
 			super.checkInputBoxHasValue("begin", begin);
 			super.checkInputBoxHasValue("end", end);
