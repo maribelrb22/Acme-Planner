5c52a4eb5743876c60530dcbca0be8d888a2ecb7
mrb <62081471+maribelrb22@users.noreply.github.com>
2021-05-17 02:09:12 +0200

    Tests AnonymousWorkPlan
    No coverage or comments

---
+++ src/test/java/acme/testing/anonymous/workPlan/AnonymousWorkPlanListTest.java
@@ -0,0 +1,24 @@
+package acme.testing.anonymous.workPlan;
+
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class AnonymousWorkPlanListTest extends AcmePlannerTest {
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/workPlan/list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void listWorkPlan(final int recordIndex, final String title, final String begin, final String end) {
+		
+		super.clickOnMenu("Anonymous", "Workplan list");
+		
+		super.checkColumnHasValue(recordIndex, 0, title);
+		super.checkColumnHasValue(recordIndex, 1, begin);
+		super.checkColumnHasValue(recordIndex, 2, end);
+		
+	}
+}
---
+++ src/test/java/acme/testing/anonymous/workPlan/AnonymousWorkPlanShowTest.java
@@ -0,0 +1,24 @@
+package acme.testing.anonymous.workPlan;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class AnonymousWorkPlanShowTest extends AcmePlannerTest{
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/workPlan/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void showPositiveWorkPlan(final int recordIndex, final String title, final String begin, final String end, final String workload) {
+		
+		super.clickOnMenu("Anonymous", "Workplan list");
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("begin", begin);
+		super.checkInputBoxHasValue("end", end);
+		super.checkInputBoxHasValue("workload", workload);
+	}
+}
