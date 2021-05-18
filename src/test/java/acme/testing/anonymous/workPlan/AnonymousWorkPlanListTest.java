f2fdd0a0f27904857b8b37ab6ce8756a53b0c5e0
mrb <62081471+maribelrb22@users.noreply.github.com>
2021-05-18 13:36:49 +0200

    Unification of tests

--- src/test/java/acme/testing/anonymous/workPlan/AnonymousWorkPlanListTest.java
+++ src/test/java/acme/testing/anonymous/workPlan/AnonymousWorkPlanListTest.java
@@ -11,17 +11,24 @@ public class AnonymousWorkPlanListTest extends AcmePlannerTest {
 	
 	// Test cases -------------------------------------------------------------
 	
-	// This test case check that if you enter as anonymous in workPlan list, the columns found are the expected.
+	// This test case check that if you enter as anonymous in workPlan list, the columns and the type of the different input boxes found are the expected.
 	@ParameterizedTest
-	@CsvFileSource(resources = "/anonymous/workPlan/list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/anonymous/workPlan/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void listWorkPlan(final int recordIndex, final String title, final String begin, final String end) {
+	public void listWorkPlan(final int recordIndex, final String title, final String begin, final String end, final String workload) {
 		
 		super.clickOnMenu("Anonymous", "Workplan list");
 		
 		super.checkColumnHasValue(recordIndex, 0, title);
 		super.checkColumnHasValue(recordIndex, 1, begin);
 		super.checkColumnHasValue(recordIndex, 2, end);
+		
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("begin", begin);
+		super.checkInputBoxHasValue("end", end);
+		super.checkInputBoxHasValue("workload", workload);
 		
 	}
 }
