9183d54f59f029794e4d5c2d5bce72b8419d5057
JAVIER MARTÍNEZ FERNÁNDEZ <javmarfer3@alum.us.es>
2021-05-25 00:12:35 +0200

    Add improves
    Improves about tasks and workplans

--- src/main/java/acme/features/anonymous/workPlan/AnonymousWorkPlanShowService.java
+++ src/main/java/acme/features/anonymous/workPlan/AnonymousWorkPlanShowService.java
@@ -1,12 +1,13 @@
 package acme.features.anonymous.workPlan;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import acme.entities.workPlan.WorkPlan;
 import acme.framework.components.Model;
 import acme.framework.components.Request;
 import acme.framework.entities.Anonymous;
 import acme.framework.services.AbstractShowService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 @Service
 public class AnonymousWorkPlanShowService implements AbstractShowService<Anonymous, WorkPlan> {
@@ -15,23 +16,23 @@ public class AnonymousWorkPlanShowService implements AbstractShowService<Anonymo
     AnonymousWorkPlanRepository anonymousWorkPlanRepository;
 
     @Override
-    public boolean authorise(Request<WorkPlan> request) {
+    public boolean authorise(final Request<WorkPlan> request) {
         assert request != null;
         return true;
     }
 
     @Override
-    public void unbind(Request<WorkPlan> request, WorkPlan entity, Model model) {
+    public void unbind(final Request<WorkPlan> request, final WorkPlan entity, final Model model) {
         assert request != null;
         assert entity != null;
         assert model != null;
         model.setAttribute("workload", entity.getWorkload());
         model.setAttribute("id", entity.getId());
-        request.unbind(entity, model, "title", "begin", "end", "workload","id");
+        request.unbind(entity, model, "title", "begin", "end", "workload", "id", "executionPeriod", "tasks");
     }
 
     @Override
-    public WorkPlan findOne(Request<WorkPlan> request) {
+    public WorkPlan findOne(final Request<WorkPlan> request) {
         assert request != null;
         WorkPlan result;
         int id;
@@ -40,4 +41,5 @@ public class AnonymousWorkPlanShowService implements AbstractShowService<Anonymo
 
         return result;
     }
+    
 }
